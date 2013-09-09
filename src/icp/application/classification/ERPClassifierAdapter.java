8baa7ed2b295ae2fe95eab04a44e0bfbabd89eae
trondik <trondik@kiv.zcu.cz>
2013-09-09 15:50:34 +0200

    NullPointerException instead of NotImplementedException is used

--- src/icp/application/classification/ERPClassifierAdapter.java
+++ src/icp/application/classification/ERPClassifierAdapter.java
@@ -3,8 +3,6 @@ package icp.application.classification;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
-
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 /**
  * Adapter of IERPClassifier interface. 
  * All methods throw NotImplementedException when called.
@@ -15,44 +13,44 @@ public class ERPClassifierAdapter implements IERPClassifier {
 
 	@Override
 	public void setFeatureExtraction(IFeatureExtraction fe) {
-		throw new NotImplementedException();
+		throw new NullPointerException();
 	}
 
 	@Override
 	public void train(List<double[][]> epochs, List<Double> targets,
 			int numberOfiter, IFeatureExtraction fe) {
-		throw new NotImplementedException();
+		throw new NullPointerException();
 	}
 
 	@Override
 	public ClassificationStatistics test(List<double[][]> epochs,
 			List<Double> targets) {
-		throw new NotImplementedException();
+		throw new NullPointerException();
 	}
 
 	@Override
 	public double classify(double[][] epoch) {
-		throw new NotImplementedException();
+		throw new NullPointerException();
 	}
 
 	@Override
 	public void load(InputStream is) {
-		throw new NotImplementedException();
+		throw new NullPointerException();
 	}
 
 	@Override
 	public void save(OutputStream dest) {
-		throw new NotImplementedException();
+		throw new NullPointerException();
 	}
 	
 	@Override
 	public void load(String file) {
-		throw new NotImplementedException();
+		throw new NullPointerException();
 	}
 
 	@Override
 	public void save(String file) {
-		throw new NotImplementedException();
+		throw new NullPointerException();
 	}
 
 }

20c923b497144f591b4f122cb874403e6fdd8ba5
LUKAS VAREKA <lvareka@kiv.zcu.cz>
2015-01-13 09:36:32 +0100

    OfflineDataProvider: File access modified.

--- src/icp/online/app/OffLineDataProvider.java
+++ src/icp/online/app/OffLineDataProvider.java
@@ -13,20 +13,25 @@ import cz.zcu.kiv.signal.EEGMarker;
 
 public class OffLineDataProvider extends Observable  implements IDataProvider{
 	
-	private File vhdrFile;
-	private File vmrkFile;
+	private String vhdrFile;
+	private String vmrkFile;
 	private final int FZ_INDEX = 17;
 	private final int CZ_INDEX = 18;
 	private final int PZ_INDEX = 19;
 	
-	public OffLineDataProvider(File vhdrFile, File markerFile) {
+	public OffLineDataProvider(String vhdrFile, String markerFile) {
 		this.vhdrFile = vhdrFile;
 		this.vmrkFile = markerFile;
 	}
 
 	public OffLineDataProvider(File eegFile) {
-		// TODO Auto-generated constructor stub
+		int index = eegFile.getAbsolutePath().lastIndexOf(".");
 		
+		String ext = eegFile.getAbsolutePath().substring(index);
+		String baseName = eegFile.getAbsolutePath().substring(0, index);
+		
+		this.vhdrFile = baseName + ".vhrd";
+		this.vmrkFile = baseName + ".vmrk";
 	}
 
 	@Override
@@ -37,10 +42,10 @@ public class OffLineDataProvider extends Observable  implements IDataProvider{
 		DataTransformer dt = new EEGDataTransformer();
 		
 		try {
-			double[] fzChannel = dt.readBinaryData(vhdrFile.getAbsolutePath(), FZ_INDEX);
-			double[] czChannel = dt.readBinaryData(vhdrFile.getAbsolutePath(), CZ_INDEX);
-			double[] pzChannel = dt.readBinaryData(vhdrFile.getAbsolutePath(), PZ_INDEX);
-			Map<String, EEGMarker> markers = dt.readMarkers(vmrkFile.getAbsolutePath());
+			double[] fzChannel = dt.readBinaryData(vhdrFile, FZ_INDEX);
+			double[] czChannel = dt.readBinaryData(vhdrFile, CZ_INDEX);
+			double[] pzChannel = dt.readBinaryData(vhdrFile, PZ_INDEX);
+			Map<String, EEGMarker> markers = dt.readMarkers(vmrkFile);
 			
 			
 		} catch (IOException e) {

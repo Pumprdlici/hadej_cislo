c86e9f63a444a17fe52df680874d449f45088d69
LUKAS VAREKA <lvareka@kiv.zcu.cz>
2015-01-20 10:26:00 +0100

    Const.java upgraded.

--- src/icp/Const.java
+++ src/icp/Const.java
@@ -153,4 +153,7 @@ public class Const {
     public static final String DEF_IP_ADDRESS = "147.228.127.95";
     public static final String[] DEF_PORTS = {"51244"};
     public static final int DEF_PORT = 51244;
+    
+    public static final int USED_CHANNELS = 3;
+    public static final int GUESSED_NUMBERS = 9;
 }

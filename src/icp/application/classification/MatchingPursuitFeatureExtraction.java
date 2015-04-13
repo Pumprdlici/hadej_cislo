221234621a062b1dadb5bd291d77b49cf768267e
Thorsky <silhavyk@students.zcu.cz>
2015-04-13 21:19:57 +0200

    Adding Matching Pursuit extractor.

---
+++ src/icp/application/classification/MatchingPursuitFeatureExtraction.java
@@ -0,0 +1,100 @@
+package icp.application.classification;
+
+import cz.zcu.kiv.eegdsp.matchingpursuit.MatchingPursuit;
+
+/**
+ * Feature extractor using Matching Pursuit algorithm implemented in eegdsp library.
+ * @author Karel Silhavy
+ *
+ */
+public class MatchingPursuitFeatureExtraction implements IFeatureExtraction {
+
+	/**
+	 * EEG channels to be transformed to feature vectors
+	 */
+	private static final int[] CHANNELS = { 1, 2, 3 };
+
+	/**
+	 * Number of samples to be used - Fs = 1000 Hz expected
+	 */
+	private static final int EPOCH_SIZE = 1024;
+	
+	/**
+	 * Subsampling factor
+	 */
+	private static final int DOWN_SMPL_FACTOR = 8;
+
+	
+	/**
+	 * Skip initial samples in each epoch
+	 */
+	private static final int SKIP_SAMPLES = 0;
+	
+	/**
+	 * Private instance of singleton.
+	 */
+	private MatchingPursuit instance;
+		
+	/**
+	 * Prepare instance for use.
+	 * Default number of iteration is 1.
+	 */
+	public MatchingPursuitFeatureExtraction() {
+		this.instance = MatchingPursuit.getInstance();
+		this.instance.setIterationCount(1);
+	}
+	
+	@Override
+	public double[] extractFeatures(double[][] epoch) {
+		
+		int numberOfChannels = CHANNELS.length;
+		double[] signal = new double[EPOCH_SIZE * numberOfChannels / DOWN_SMPL_FACTOR];
+		
+		int k = 0;
+		for(int i = 0; i < numberOfChannels; i++) {
+			for(int j = 0; j < (EPOCH_SIZE  / DOWN_SMPL_FACTOR); j++) {
+				signal[k] = epoch[i][j + SKIP_SAMPLES];
+				k++;
+			}
+		}
+		
+		return instance.processSignal(signal).getReconstruction();
+		
+	}
+
+	
+	@Override
+	public int getFeatureDimension() {
+		int lenghtOfFeatures = EPOCH_SIZE * CHANNELS.length  / DOWN_SMPL_FACTOR;
+		int i = 1;
+		while(lenghtOfFeatures > Math.pow(2, i)) {
+			i++;
+		}
+		return (int) Math.pow(2, i);
+	}
+	
+	
+	/**
+	 * Sets number of iterations.
+	 * 
+	 * Each iteration adds one atom.
+	 * 
+	 * @param iterationCount number of iterations
+	 */
+	public void setIterationCount(int iterationCount) {
+		if (iterationCount < 1) {
+			throw new IllegalArgumentException("Number of iterations must be >= 1");
+		}
+		this.instance.setIterationCount(iterationCount);
+	}
+	
+	/**
+	 * Gets number of iterations.
+	 * 
+	 * @return iterationCount number of iterations
+	 */
+	public int getIterationCount() {
+		return this.instance.getIterationCount();
+	}
+
+}

05df0485449ecc393ee53fda268a9036794d7a57
Jaroslav Klaus <buzz3333@gmail.com>
2015-04-22 21:40:05 +0200

    Changes in GUI

--- src/icp/online/gui/ChangeFeatureExtractionFrame.java
+++ src/icp/online/gui/ChangeFeatureExtractionFrame.java
@@ -1,395 +1,770 @@
-package icp.online.gui;
+Qckage icp.online.gui;
+
+
 
 import icp.Const;
 
+
+
 import java.awt.Dimension;
+
 import java.awt.GridLayout;
+
 import java.awt.Toolkit;
+
 import java.awt.event.ActionEvent;
+
 import java.awt.event.ActionListener;
+
 import java.awt.event.KeyEvent;
 
+
+
 import javax.swing.BorderFactory;
+
 import javax.swing.Box;
+
 import javax.swing.BoxLayout;
+
 import javax.swing.ButtonGroup;
+
 import javax.swing.JButton;
+
 import javax.swing.JComponent;
+
+import javax.swing.JDialog;
+
 import javax.swing.JFrame;
+
 import javax.swing.JLabel;
+
 import javax.swing.JOptionPane;
+
 import javax.swing.JPanel;
+
 import javax.swing.JRadioButton;
+
 import javax.swing.JSpinner;
+
 import javax.swing.KeyStroke;
+
 import javax.swing.SpinnerNumberModel;
 
+
+
 @SuppressWarnings("serial")
+
 public class ChangeFeatureExtractionFrame extends JFrame {
 
+
+
 	private MainFrame mainFrame;
+
 	private JSpinner fasEpochSpinner;
+
 	private JSpinner fasSubsampleSpinner;
+
 	private JSpinner fasSkipSpinner;
+
 	private JSpinner wtEpochSpinner;
+
 	private JSpinner wtSubsampleSpinner;
+
 	private JSpinner wtSkipSpinner;
+
 	private JSpinner waveletNameSpinner;
+
 	private JSpinner mpEpochSpinner;
+
 	private JSpinner mpSubsampleSpinner;
+
 	private JSpinner mpSkipSpinner;
+
 	private JSpinner hhtEpochSpinner;
+
 	private JSpinner hhtSubsampleSpinner;
+
 	private JSpinner hhtSkipSpinner;
+
 	private final SpinnerNumberModel epochSnm = new SpinnerNumberModel(1, 1,
+
 			Const.POSTSTIMULUS_VALUES, 1);
+
 	private final SpinnerNumberModel subsampleSnm = new SpinnerNumberModel(0,
+
 			0, 512, 1);
+
 	private final SpinnerNumberModel skipSnm = new SpinnerNumberModel(0, 0,
+
 			512, 1);
+
 	private JRadioButton fasBttn;
+
 	private JRadioButton wtBttn;
+
 	private JRadioButton mpBttn;
+
 	private JRadioButton hhtBttn;
 
+
+
 	public ChangeFeatureExtractionFrame(MainFrame mainFrame) {
+
 		super("Choose Feature Extractor and Its Parameters");
+
+
 
 		this.mainFrame = mainFrame;
 
+
+
 		this.getContentPane().add(createClassifierFrame());
 
+
+
 		this.setVisible(false);
+
 		this.pack();
 
+
+
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+
 		// TODO this.setSize(Const.MAIN_WINDOW_WIDTH, Const.MAIN_WINDOW_HEIGHT);
+
 		this.setSize(900, 600);
+
 		this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height
+
 				/ 2 - this.getSize().height / 2);
+
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
 		setEscListener(this);
+
 	}
+
+
 
 	private void setEscListener(final ChangeFeatureExtractionFrame frame) {
+
 		ActionListener escListener = new ActionListener() {
 
+
+
 			@Override
+
 			public void actionPerformed(ActionEvent e) {
+
 				frame.dispose();
+
 			}
+
 		};
 
+
+
 		this.getRootPane().registerKeyboardAction(escListener,
+
 				KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+
 				JComponent.WHEN_IN_FOCUSED_WINDOW);
+
 	}
+
+
 
 	private JPanel createClassifierFrame() {
+
 		GridLayout mainLayout = new GridLayout(1, 2);
+
 		JPanel contentJP = new JPanel(mainLayout);
 
+
+
 		contentJP.add(createRadioBttns());
+
 		contentJP.add(createParameters());
 
+
+
 		return contentJP;
+
 	}
+
+
 
 	private JPanel createRadioBttns() {
+
 		fasBttn = new JRadioButton("Filter and Subsample");
+
 		fasBttn.setSelected(false);
+
 		fasBttn.addActionListener(new ActionListener() {
 
+
+
 			@Override
+
 			public void actionPerformed(ActionEvent e) {
+
 				fasEpochSpinner.setEnabled(true);
+
 				fasSubsampleSpinner.setEnabled(true);
+
 				fasSkipSpinner.setEnabled(true);
+
 				wtEpochSpinner.setEnabled(false);
+
 				wtSubsampleSpinner.setEnabled(false);
+
 				wtSkipSpinner.setEnabled(false);
+
 				waveletNameSpinner.setEnabled(false);
+
 				mpEpochSpinner.setEnabled(false);
+
 				mpSubsampleSpinner.setEnabled(false);
+
 				mpSkipSpinner.setEnabled(false);
+
 				hhtEpochSpinner.setEnabled(false);
+
 				hhtSubsampleSpinner.setEnabled(false);
+
 				hhtSkipSpinner.setEnabled(false);
+
 			}
+
 		});
+
+
 
 		wtBttn = new JRadioButton("Wavelet Transform");
+
 		wtBttn.setSelected(false);
+
 		wtBttn.addActionListener(new ActionListener() {
 
+
+
 			@Override
+
 			public void actionPerformed(ActionEvent e) {
+
 				fasEpochSpinner.setEnabled(false);
+
 				fasSubsampleSpinner.setEnabled(false);
+
 				fasSkipSpinner.setEnabled(false);
+
 				wtEpochSpinner.setEnabled(true);
+
 				wtSubsampleSpinner.setEnabled(true);
+
 				wtSkipSpinner.setEnabled(true);
+
 				waveletNameSpinner.setEnabled(true);
+
 				mpEpochSpinner.setEnabled(false);
+
 				mpSubsampleSpinner.setEnabled(false);
+
 				mpSkipSpinner.setEnabled(false);
+
 				hhtEpochSpinner.setEnabled(false);
+
 				hhtSubsampleSpinner.setEnabled(false);
+
 				hhtSkipSpinner.setEnabled(false);
+
 			}
+
 		});
+
+
 
 		mpBttn = new JRadioButton("Matching Pursuit");
+
 		mpBttn.setSelected(false);
+
 		mpBttn.addActionListener(new ActionListener() {
 
+
+
 			@Override
+
 			public void actionPerformed(ActionEvent e) {
+
 				fasEpochSpinner.setEnabled(false);
+
 				fasSubsampleSpinner.setEnabled(false);
+
 				fasSkipSpinner.setEnabled(false);
+
 				wtEpochSpinner.setEnabled(false);
+
 				wtSubsampleSpinner.setEnabled(false);
+
 				wtSkipSpinner.setEnabled(false);
+
 				waveletNameSpinner.setEnabled(false);
+
 				mpEpochSpinner.setEnabled(true);
+
 				mpSubsampleSpinner.setEnabled(true);
+
 				mpSkipSpinner.setEnabled(true);
+
 				hhtEpochSpinner.setEnabled(false);
+
 				hhtSubsampleSpinner.setEnabled(false);
+
 				hhtSkipSpinner.setEnabled(false);
+
 			}
+
 		});
+
+
 
 		hhtBttn = new JRadioButton("Hilbert-Huang Transform");
+
 		hhtBttn.setSelected(false);
+
 		hhtBttn.addActionListener(new ActionListener() {
 
+
+
 			@Override
+
 			public void actionPerformed(ActionEvent e) {
+
 				fasEpochSpinner.setEnabled(false);
+
 				fasSubsampleSpinner.setEnabled(false);
+
 				fasSkipSpinner.setEnabled(false);
+
 				wtEpochSpinner.setEnabled(false);
+
 				wtSubsampleSpinner.setEnabled(false);
+
 				wtSkipSpinner.setEnabled(false);
+
 				waveletNameSpinner.setEnabled(false);
+
 				mpEpochSpinner.setEnabled(false);
+
 				mpSubsampleSpinner.setEnabled(false);
+
 				mpSkipSpinner.setEnabled(false);
+
 				hhtEpochSpinner.setEnabled(true);
+
 				hhtSubsampleSpinner.setEnabled(true);
+
 				hhtSkipSpinner.setEnabled(true);
+
 			}
+
 		});
 
+
+
 		ButtonGroup group = new ButtonGroup();
+
 		group.add(fasBttn);
+
 		group.add(wtBttn);
+
 		group.add(mpBttn);
+
 		group.add(hhtBttn);
 
+
+
 		JPanel pane = new JPanel();
+
 		pane.setBorder(BorderFactory.createTitledBorder("Feature Extraction"));
+
 		pane.setLayout(new BoxLayout(pane, BoxLayout.PAGE_AXIS));
+
 		pane.add(fasBttn);
+
 		pane.add(wtBttn);
+
 		pane.add(mpBttn);
+
 		pane.add(hhtBttn);
 
+
+
 		return pane;
+
 	}
+
+
 
 	private JPanel createParameters() {
 
+
+
 		// Filter and Subsample
+
 		JPanel fasPane = createFasPane();
 
+
+
 		// Wavelet Transform
+
 		JPanel wtPane = createWtPane();
 
+
+
 		// Matching Pursuit
+
 		JPanel mpPane = createMpPane();
 
+
+
 		// Hilbert-Huang Transform
+
 		JPanel hhtPane = createHhtPane();
+
+
 
 		JPanel bttnPane = createBttnPane();
 
+
+
 		JPanel pane = new JPanel();
+
 		pane.setBorder(BorderFactory.createTitledBorder("Parameters"));
+
 		pane.setLayout(new BoxLayout(pane, BoxLayout.PAGE_AXIS));
+
 		pane.add(fasPane);
+
 		pane.add(wtPane);
+
 		pane.add(mpPane);
+
 		pane.add(hhtPane);
+
 		pane.add(bttnPane);
 
+
+
 		return pane;
+
 	}
+
+
 
 	private JPanel createFasPane() {
+
 		JPanel fasPane = new JPanel();
+
 		fasPane.setBorder(BorderFactory
+
 				.createTitledBorder("Filter and Subsample"));
+
 		fasPane.setLayout(new GridLayout(0, 2));
 
+
+
 		JLabel fasEpochLabel = new JLabel("Epoch Size");
+
 		fasPane.add(fasEpochLabel);
+
 		fasEpochSpinner = new JSpinner(epochSnm);
+
 		fasEpochSpinner.setEnabled(false);
+
 		fasPane.add(fasEpochSpinner);
 
+
+
 		JLabel fasSubsampleLable = new JLabel("Subsampling Factor");
+
 		fasPane.add(fasSubsampleLable);
+
 		fasSubsampleSpinner = new JSpinner(subsampleSnm);
+
 		fasSubsampleSpinner.setEnabled(false);
+
 		fasPane.add(fasSubsampleSpinner);
 
+
+
 		JLabel fasSkipLabel = new JLabel("Skip Samples");
+
 		fasPane.add(fasSkipLabel);
+
 		fasSkipSpinner = new JSpinner(skipSnm);
+
 		fasSkipSpinner.setEnabled(false);
+
 		fasPane.add(fasSkipSpinner);
+
 		return fasPane;
+
 	}
+
+
 
 	private JPanel createWtPane() {
+
 		JPanel wtPane = new JPanel();
+
 		wtPane.setBorder(BorderFactory.createTitledBorder("Wavelet Transform"));
+
 		wtPane.setLayout(new GridLayout(0, 2));
 
+
+
 		JLabel wtEpochLabel = new JLabel("Epoch Size");
+
 		wtPane.add(wtEpochLabel);
+
 		wtEpochSpinner = new JSpinner(epochSnm);
+
 		wtEpochSpinner.setEnabled(false);
+
 		wtPane.add(wtEpochSpinner);
 
+
+
 		JLabel wtSubsampleLable = new JLabel("Subsampling Factor");
+
 		wtPane.add(wtSubsampleLable);
+
 		wtSubsampleSpinner = new JSpinner(subsampleSnm);
+
 		wtSubsampleSpinner.setEnabled(false);
+
 		wtPane.add(wtSubsampleSpinner);
 
+
+
 		JLabel wtSkipLabel = new JLabel("Skip Samples");
+
 		wtPane.add(wtSkipLabel);
+
 		wtSkipSpinner = new JSpinner(skipSnm);
+
 		wtSkipSpinner.setEnabled(false);
+
 		wtPane.add(wtSkipSpinner);
 
+
+
 		JLabel waveletNameLabel = new JLabel("Wavelet Name");
+
 		wtPane.add(waveletNameLabel);
+
 		SpinnerNumberModel waveletSnm = new SpinnerNumberModel(0, 0, 17, 1);
+
 		waveletNameSpinner = new JSpinner(waveletSnm);
+
 		waveletNameSpinner.setEnabled(false);
+
 		wtPane.add(waveletNameSpinner);
+
 		return wtPane;
+
 	}
+
+
 
 	private JPanel createMpPane() {
+
 		JPanel mpPane = new JPanel();
+
 		mpPane.setBorder(BorderFactory.createTitledBorder("Matching Pursuit"));
+
 		mpPane.setLayout(new GridLayout(0, 2));
 
+
+
 		JLabel mpEpochLabel = new JLabel("Epoch Size");
+
 		mpPane.add(mpEpochLabel);
+
 		mpEpochSpinner = new JSpinner(epochSnm);
+
 		mpEpochSpinner.setEnabled(false);
+
 		mpPane.add(mpEpochSpinner);
 
+
+
 		JLabel mpSubsampleLable = new JLabel("Subsampling Factor");
+
 		mpPane.add(mpSubsampleLable);
+
 		mpSubsampleSpinner = new JSpinner(subsampleSnm);
+
 		mpSubsampleSpinner.setEnabled(false);
+
 		mpPane.add(mpSubsampleSpinner);
 
+
+
 		JLabel mpSkipLabel = new JLabel("Skip Samples");
+
 		mpPane.add(mpSkipLabel);
+
 		mpSkipSpinner = new JSpinner(skipSnm);
+
 		mpSkipSpinner.setEnabled(false);
+
 		mpPane.add(mpSkipSpinner);
+
 		return mpPane;
+
 	}
+
+
 
 	private JPanel createHhtPane() {
+
 		JPanel hhtPane = new JPanel();
+
 		hhtPane.setBorder(BorderFactory
+
 				.createTitledBorder("Hilbert-Huang Transform"));
+
 		hhtPane.setLayout(new GridLayout(0, 2));
 
+
+
 		JLabel hhtEpochLabel = new JLabel("Epoch Size");
+
 		hhtPane.add(hhtEpochLabel);
+
 		hhtEpochSpinner = new JSpinner(epochSnm);
+
 		hhtEpochSpinner.setEnabled(false);
+
 		hhtPane.add(hhtEpochSpinner);
 
+
+
 		JLabel hhtSubsampleLable = new JLabel("Subsampling Factor");
+
 		hhtPane.add(hhtSubsampleLable);
+
 		hhtSubsampleSpinner = new JSpinner(subsampleSnm);
+
 		hhtSubsampleSpinner.setEnabled(false);
+
 		hhtPane.add(hhtSubsampleSpinner);
 
+
+
 		JLabel hhtSkipLabel = new JLabel("Skip Samples");
+
 		hhtPane.add(hhtSkipLabel);
+
 		hhtSkipSpinner = new JSpinner(skipSnm);
+
 		hhtSkipSpinner.setEnabled(false);
+
 		hhtPane.add(hhtSkipSpinner);
+
 		return hhtPane;
+
 	}
+
+
 
 	private JPanel createBttnPane() {
-		JPanel bttnPane = new JPanel();
-		final JButton trainBttn = new JButton("Train");
-		trainBttn.addActionListener(new ActionListener() {
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("training");
-			}
-		});
-		JButton okBttn = new JButton("OK");
+		JPanel bttnPane = new JPanel();
+
+		JButton okBttn = new JButton("Next");
+
 		final ChangeFeatureExtractionFrame c = this;
+
 		okBttn.addActionListener(new ActionListener() {
 
+
+
 			@Override
+
 			public void actionPerformed(ActionEvent e) {
+
 				// TODO how to create the classifier with its parameters
-				if(fasBttn.isSelected()) {
+
+				if (fasBttn.isSelected()) {
+
 					mainFrame.setTrained(false);
-					
-				} else if(wtBttn.isSelected()) {
+
+
+
+					c.dispose();
+
+					ChangeClassifierFrame cc = new ChangeClassifierFrame(mainFrame);
+
+					cc.setVisible(true);
+
+				} else if (wtBttn.isSelected()) {
+
 					mainFrame.setTrained(false);
-					
-				} else if(mpBttn.isSelected()) {
+
+
+
+					c.dispose();
+
+					ChangeClassifierFrame cc = new ChangeClassifierFrame(mainFrame);
+
+					cc.setVisible(true);
+
+				} else if (mpBttn.isSelected()) {
+
 					mainFrame.setTrained(false);
-					
-				} else if(hhtBttn.isSelected()) {
+
+
+
+					c.dispose();
+
+					ChangeClassifierFrame cc = new ChangeClassifierFrame(mainFrame);
+
+					cc.setVisible(true);
+
+				} else if (hhtBttn.isSelected()) {
+
 					mainFrame.setTrained(false);
-					
+
+
+
+					c.dispose();
+
+					ChangeClassifierFrame cc = new ChangeClassifierFrame(mainFrame);
+
+					cc.setVisible(true);
+
+				} else {
+
+					JOptionPane.showMessageDialog(null, "Choose one Feature Extraction method and fill in its parameters");
+
 				}
-				
-				if (mainFrame.isTrained() == false) {
-					int dialogResult = JOptionPane
-							.showConfirmDialog(
-									null,
-									"You have to train the classifier in order to use it\nWould you like to train it now?",
-									"Classifier is not trained",
-									JOptionPane.YES_NO_OPTION);
-					if (dialogResult == JOptionPane.YES_OPTION) {
-						c.dispose();
-						trainBttn.doClick();
-						mainFrame.setTrained(true);
-					} else {
-						c.dispose();
-					}
-				}
+
 			}
+
 		});
+
 		bttnPane.setLayout(new BoxLayout(bttnPane, BoxLayout.LINE_AXIS));
+
 		bttnPane.add(Box.createHorizontalGlue());
-		bttnPane.add(trainBttn);
+
 		bttnPane.add(okBttn);
+
 		return bttnPane;
+
 	}
+
 }
+
--- src/icp/online/gui/MainFrame.java
+++ src/icp/online/gui/MainFrame.java
@@ -1,376 +1,729 @@
-package icp.online.gui;
+Qckage icp.online.gui;
+
+
 
 import icp.Const;
+
 import icp.application.classification.FilterAndSubsamplingFeatureExtraction;
+
 import icp.application.classification.IERPClassifier;
+
 import icp.application.classification.IFeatureExtraction;
+
 import icp.application.classification.MLPClassifier;
+
 import icp.online.app.IDataProvider;
+
 import icp.online.app.OffLineDataProvider;
+
 import icp.online.app.OnLineDataProvider;
+
 import icp.online.app.OnlineDetection;
 
+
+
 import java.awt.Color;
+
 import java.awt.Dimension;
+
 import java.awt.Font;
+
 import java.awt.GridLayout;
+
 import java.awt.Toolkit;
+
 import java.awt.event.ActionEvent;
+
 import java.awt.event.ActionListener;
+
 import java.awt.event.KeyEvent;
+
 import java.io.File;
+
 import java.util.Arrays;
+
 import java.util.Comparator;
+
 import java.util.Observable;
+
 import java.util.Observer;
 
+
+
 import javax.swing.AbstractAction;
+
 import javax.swing.JFileChooser;
+
 import javax.swing.JFrame;
+
 import javax.swing.JMenu;
+
 import javax.swing.JMenuBar;
+
 import javax.swing.JMenuItem;
+
 import javax.swing.JOptionPane;
+
 import javax.swing.JPanel;
+
 import javax.swing.JScrollPane;
+
 import javax.swing.JTable;
+
 import javax.swing.JTextPane;
+
 import javax.swing.KeyStroke;
+
 import javax.swing.filechooser.FileNameExtensionFilter;
+
 import javax.swing.table.AbstractTableModel;
+
 import javax.swing.text.SimpleAttributeSet;
+
 import javax.swing.text.StyleConstants;
+
 import javax.swing.text.StyledDocument;
 
+
+
 import org.apache.log4j.BasicConfigurator;
+
 import org.apache.log4j.Logger;
 
+
+
 @SuppressWarnings("serial")
+
 public class MainFrame extends JFrame implements Observer {
+
+
 
 	private AbstractTableModel data;
 
+
+
 	private JTextPane winnerJTA;
+
+
 
 	private JFileChooser chooser;
 
+
+
 	private Observer detection;
+
+
 
 	private IDataProvider dp;
 
+
+
 	public File eegFile;
+
+
 
 	private Thread dataProvider;
 
+
+
 	private final Logger log;
+
+
 
 	private IFeatureExtraction fe;
 
+
+
 	private IERPClassifier classifier;
+
+
 
 	private final ShowChart epochCharts;
 
+
+
 	private boolean trained = false;
 
+
+
 	public MainFrame() {
+
 		super(Const.APP_NAME);
+
 		BasicConfigurator.configure();
+
 		log = Logger.getLogger(MainFrame.class);
+
 		epochCharts = new ShowChart(this);
 
+
+
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
 		getContentPane().add(createContentJP());
+
 		this.setJMenuBar(createMenu());
 
+
+
 		this.setVisible(true);
+
 		this.pack();
+
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+
 		this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height
+
 				/ 2 - this.getSize().height / 2);
+
 		this.setSize(Const.MAIN_WINDOW_WIDTH, Const.MAIN_WINDOW_HEIGHT);
+
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+
+
 		classifier = new MLPClassifier();
+
 		classifier.load(Const.TRAINING_FILE_NAME);
+
 		fe = new FilterAndSubsamplingFeatureExtraction();
+
 		classifier.setFeatureExtraction(fe);
+
 	}
+
+
 
 	private JMenuBar createMenu() {
+
 		JMenuBar menuBar = new JMenuBar();
+
 		JMenu fileMenu = new JMenu("File");
+
 		JMenuItem offlineMenuItem = new JMenuItem();
+
 		offlineMenuItem.setAction((new LoadOfflineData()));
+
 		JMenuItem onlineMenuItem = new JMenuItem();
+
 		onlineMenuItem.setAction(new LoadOnlineData());
+
 		JMenuItem chartMenuItem = new JMenuItem();
+
 		chartMenuItem.setAction(this.epochCharts);
+
 		JMenuItem endMenuItem = new JMenuItem("Close");
+
 		endMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q,
+
 				ActionEvent.CTRL_MASK));
+
 		endMenuItem.addActionListener(new ActionListener() {
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-			}
-		});
-		final MainFrame mf = this;
-		JMenu settingsMenu = new JMenu("Settings");
-		JMenuItem classifierMenuItem = new JMenuItem("Classifier");
-		classifierMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_K,
-				ActionEvent.CTRL_MASK));
-		classifierMenuItem.addActionListener(new ActionListener() {
+
 
 			@Override
+
 			public void actionPerformed(ActionEvent e) {
-				ChangeClassifierFrame c = new ChangeClassifierFrame(mf);
-				c.setVisible(true);
+
+				System.exit(0);
+
 			}
+
 		});
-		JMenuItem featureMenuItem = new JMenuItem("Feature Extraction");
+
+		final MainFrame mf = this;
+
+		JMenu settingsMenu = new JMenu("Settings");
+
+		JMenuItem featureMenuItem = new JMenuItem("Feature Extraction and Classifier");
+
 		featureMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F,
+
 				ActionEvent.CTRL_MASK));
+
 		featureMenuItem.addActionListener(new ActionListener() {
 
+
+
 			@Override
+
 			public void actionPerformed(ActionEvent e) {
+
 				ChangeFeatureExtractionFrame f = new ChangeFeatureExtractionFrame(
+
 						mf);
+
 				f.setVisible(true);
+
 			}
+
 		});
 
+
+
 		menuBar.add(fileMenu);
+
 		fileMenu.add(onlineMenuItem);
+
 		fileMenu.add(offlineMenuItem);
+
 		fileMenu.add(chartMenuItem);
+
 		fileMenu.addSeparator();
+
 		fileMenu.add(endMenuItem);
 
+
+
 		menuBar.add(settingsMenu);
-		settingsMenu.add(classifierMenuItem);
+
 		settingsMenu.add(featureMenuItem);
 
+
+
 		return menuBar;
+
 	}
+
+
 
 	private JPanel createContentJP() {
+
 		GridLayout mainLayout = new GridLayout(1, 2);
+
 		JPanel contentJP = new JPanel(mainLayout);
+
 		contentJP.add(createStimuliJT());
+
 		contentJP.add(createWinnerJTA());
 
+
+
 		return contentJP;
+
 	}
+
+
 
 	private JTextPane createWinnerJTA() {
+
 		winnerJTA = new JTextPane();
+
 		Font font = new Font(Const.RESULT_FONT_NAME, Const.RESULT_FONT_STYLE,
+
 				Const.RESULT_FONT_SIZE);
+
 		winnerJTA.setFont(font);
+
 		winnerJTA.setBackground(Color.BLACK);
+
 		winnerJTA.setForeground(Color.WHITE);
+
 		StyledDocument doc = winnerJTA.getStyledDocument();
+
 		SimpleAttributeSet center = new SimpleAttributeSet();
+
 		StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+
 		doc.setParagraphAttributes(0, doc.getLength(), center, false);
+
 		winnerJTA.setText(Const.UNKNOWN_RESULT);
+
 		return winnerJTA;
+
 	}
+
+
 
 	private JScrollPane createStimuliJT() {
+
 		data = new StimuliTableModel();
+
 		JTable stimuliJT = new JTable(data);
+
 		JScrollPane jsp = new JScrollPane(stimuliJT);
+
 		stimuliJT.setFillsViewportHeight(true);
 
+
+
 		return jsp;
+
 	}
+
+
 
 	private void initProbabilities(double[] probabilities) {
+
 		Integer[] ranks = new Integer[probabilities.length];
+
 		for (int i = 0; i < ranks.length; ++i) {
+
 			ranks[i] = i;
+
 		}
+
 		Comparator<Integer> gc = new ProbabilityComparator(probabilities);
+
 		Arrays.sort(ranks, gc);
 
+
+
 		winnerJTA.setText(String.valueOf(ranks[0] + 1));
+
 		for (int i = 0; i < probabilities.length; i++) {
+
 			data.setValueAt(probabilities[ranks[i]], i, 1);
+
 			data.setValueAt(ranks[i] + 1, i, 0);
+
 		}
 
+
+
 		this.validate();
+
 		this.repaint();
+
 	}
 
+
+
 	@Override
+
 	public void update(Observable sender, Object message)
+
 			throws IllegalArgumentException {
+
 		if (message instanceof OnlineDetection) {
+
 			double[] probabilities = ((OnlineDetection) message)
+
 					.getWeightedResults();
+
+
 
 			initProbabilities(probabilities);
 
+
+
 			this.epochCharts.update(((OnlineDetection) message).getPzAvg());
+
 		} /*
+
 		 * else { log.error(MainFrame.class.toString() +
+
 		 * ": Expencted online detection, but received something else."); throw
+
 		 * new IllegalArgumentException(
+
 		 * "Expencted online detection, but received something else."); }
+
 		 */
+
 	}
+
+
 
 	private void initGui() {
+
 		double[] zeros = new double[data.getRowCount()];
+
 		Arrays.fill(zeros, 0);
+
 		initProbabilities(zeros);
+
 		winnerJTA.setText(Const.UNKNOWN_RESULT);
+
 	}
+
+
 
 	private void stopRunningThread() {
+
 		try {
+
 			if (dataProvider != null) {
+
 				if (dp != null) {
+
 					dp.stop();
+
 				} else {
+
 					dataProvider.interrupt();
+
 				}
+
 			}
+
 		} catch (Exception ex) {
 
+
+
 		}
+
 	}
+
+
 
 	public IERPClassifier getClassifier() {
+
 		return classifier;
+
 	}
+
+
 
 	public void setClassifier(IERPClassifier classifier) {
+
 		this.classifier = classifier;
+
 	}
+
+
 
 	public IFeatureExtraction getFe() {
+
 		return fe;
+
 	}
+
+
 
 	public void setFe(IFeatureExtraction fe) {
+
 		this.fe = fe;
+
 	}
+
+
 
 	public boolean isTrained() {
+
 		return trained;
+
 	}
 
+
+
 	public void setTrained(boolean trained) {
+
 		this.trained = trained;
+
 	}
+
+
 
 	public class LoadOfflineData extends AbstractAction {
 
+
+
 		MainFrame mainFrame;
 
+
+
 		@Override
+
 		public void actionPerformed(ActionEvent actionevent) {
+
 			initGui();
+
 			chooser = new JFileChooser();
+
 			FileNameExtensionFilter filter = new FileNameExtensionFilter(
+
 					"EEG files .eeg", "EEG", "eeg");
+
 			chooser.addChoosableFileFilter(filter);
+
 			chooser.setFileFilter(filter);
+
 			chooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+
 			int i = chooser.showDialog(mainFrame, "Open");
+
 			if (i == 0) {
+
 				eegFile = chooser.getSelectedFile();
+
 				detection = new OnlineDetection(classifier, mainFrame);
+
 				stopRunningThread();
 
+
+
 				try {
+
 					dp = new OffLineDataProvider(eegFile, detection);
+
 					dataProvider = new Thread((OffLineDataProvider) dp);
+
 					dataProvider.start();
+
 				} catch (Exception ex) {
+
 					JOptionPane.showMessageDialog(mainFrame, ex.getMessage(),
+
 							"Error", JOptionPane.ERROR_MESSAGE);
+
 				}
+
 			}
+
 		}
 
+
+
 		public LoadOfflineData() {
+
 			super();
+
 			mainFrame = MainFrame.this;
+
 			putValue("AcceleratorKey", KeyStroke.getKeyStroke(KeyEvent.VK_O,
+
 					ActionEvent.CTRL_MASK));
+
 			putValue("Name", "Offline data");
+
 		}
+
 	}
+
+
 
 	public class LoadOnlineData extends AbstractAction {
 
+
+
 		MainFrame mainFrame;
 
+
+
 		@Override
+
 		public void actionPerformed(ActionEvent actionevent) {
+
 			initGui();
 
+
+
 			SetupDialogContent content = new SetupDialogContent();
+
 			int result;
+
 			boolean isOk = false;
+
 			String recorderIPAddress = null;
+
 			int port = -1;
+
 			while (!isOk) {
+
 				result = JOptionPane
+
 						.showConfirmDialog(null, content,
+
 								"Guess the Number: Setup",
+
 								JOptionPane.OK_CANCEL_OPTION,
+
 								JOptionPane.PLAIN_MESSAGE);
+
 				if (result == JOptionPane.CANCEL_OPTION) {
+
 					return;
+
 				}
+
+
 
 				recorderIPAddress = content.getIP();
+
 				port = content.getPort();
+
 				if (port != -1 && recorderIPAddress != null) {
+
 					isOk = true;
+
 				} else {
+
 					if (port == -1) {
+
 						JOptionPane.showMessageDialog(null,
+
 								"Invalid port number!", "Error",
+
 								JOptionPane.ERROR_MESSAGE);
+
 					}
+
 					if (recorderIPAddress == null) {
+
 						JOptionPane.showMessageDialog(null,
+
 								"Invalid IP address!", "Error",
+
 								JOptionPane.ERROR_MESSAGE);
+
 					}
+
 				}
+
 			}
+
+
 
 			if (isOk) {
+
 				stopRunningThread();
 
+
+
 				detection = new OnlineDetection(classifier, mainFrame);
+
 				try {
+
 					dp = new OnLineDataProvider(recorderIPAddress, port,
+
 							detection);
+
 					dataProvider = new Thread((OnLineDataProvider) dp);
+
 					dataProvider.start();
+
 				} catch (Exception ex) {
+
 					JOptionPane.showMessageDialog(mainFrame, ex.getMessage(),
+
 							"Connection Error", JOptionPane.ERROR_MESSAGE);
+
 				}
+
 			}
+
 		}
 
+
+
 		public LoadOnlineData() {
+
 			super();
+
 			mainFrame = MainFrame.this;
+
 			putValue("AcceleratorKey", KeyStroke.getKeyStroke(KeyEvent.VK_P,
+
 					ActionEvent.CTRL_MASK));
+
 			putValue("Name", "Online data");
+
 		}
+
 	}
+
 }
+
+

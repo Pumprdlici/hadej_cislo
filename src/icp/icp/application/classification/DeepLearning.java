61bef4bb4b33a7dfbc20d83e0fd812d84140c5f9
kptlipanek <mirha2009@seznam.cz>
2016-04-04 14:26:23 +0200

    Fixing errors
    imported OutputLayer from deeplearning4j

--- src/icp/icp/application/classification/DeepLearning.java
+++ src/icp/icp/application/classification/DeepLearning.java
@@ -20,7 +20,7 @@ import org.deeplearning4j.nn.api.Updater;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.layers.RBM;
-import org.deeplearning4j.nn.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.params.DefaultParamInitializer;
 import org.deeplearning4j.nn.weights.WeightInit;

84f1e784016d07aaf4c48cab57b3394bf707222d
Sedmikras <kouba.prem@gmail.com>
2016-05-02 16:07:48 +0200

    Úprava DBN zatim neni moc úspěšná(max 40 tušim)
    Další úpravy budou v buildu. Opravil sem a teď už to chce testovat a ladit.

--- src/icp/icp/application/classification/DBNClassifier.java
+++ src/icp/icp/application/classification/DBNClassifier.java
@@ -1,7 +1,6 @@
 package icp.application.classification;
 
 import org.apache.commons.io.FileUtils;
-import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.nn.api.Layer;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
@@ -10,37 +9,38 @@ import org.deeplearning4j.nn.conf.Updater;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.conf.layers.RBM;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
-import org.deeplearning4j.nn.params.DefaultParamInitializer;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
-import org.nd4j.linalg.dataset.SplitTestAndTrain;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 
 public class DBNClassifier implements IERPClassifier {
-
+    private final int ITER_DEFAULT = 50;  // Default number of iterations
+    private final int NEURON_COUNT = 9;
     private IFeatureExtraction fe;		//type of feature extraction (MatchingPursuit, FilterAndSubampling or WaveletTransform)
     private MultiLayerNetwork model;	//multi layer neural network with a logistic output layer and multiple hidden neuralNets
-    int iterations;                   //Iterations used to classify
-    private final int ITER_DEFAULT = 10; // Default number of iterations
-    
+    private int iterations;                   //Iterations used to classify
+    private int neuronCount;
+
+
     /*Default constructor*/
     public DBNClassifier() {
     	this.iterations = ITER_DEFAULT;
+        this.neuronCount = NEURON_COUNT;
     }
     
     /*Parametric constructor */
     public DBNClassifier(int iterations) {
-    	this.iterations = iterations;
+    	this.iterations = ITER_DEFAULT;
+        this.neuronCount = NEURON_COUNT;
     }
     
     /*Classifying features*/
@@ -48,20 +48,16 @@ public class DBNClassifier implements IERPClassifier {
     public double classify(double[][] epoch){
         double[] featureVector = this.fe.extractFeatures(epoch); // Extracting features to vector
         INDArray features = Nd4j.create(featureVector); // Creating INDArray with extracted features
-        double x = model.output(features, Layer.TrainingMode.TEST).getDouble(0); // Result of classifying 
-        return x;
+        return model.output(features, Layer.TrainingMode.TEST).getDouble(0); // Result of classifying
     }
 
     @Override
     public void train(List<double[][]> epochs, List<Double> targets, int numberOfIter, IFeatureExtraction fe) {
-    	// Customizing params
-        Nd4j.MAX_SLICES_TO_PRINT = -1;
-        Nd4j.MAX_ELEMENTS_PER_SLICE = -1;
         final int numRows = fe.getFeatureDimension();
         final int numColumns = 2;
-        int outputNum = 2;
         int seed = 123;
-        int listenerFreq = 1;
+        this.iterations = numberOfIter;
+        int listenerFreq = this.iterations/10;
 
         //Load Data
         double[][] outcomes = new double[targets.size()][numColumns]; // Matrix of outcomes
@@ -71,7 +67,7 @@ public class DBNClassifier implements IERPClassifier {
             double[] features = fe.extractFeatures(epoch); // Feature of each epoch
             for(int j = 0; j < numColumns; j++) {
                 outcomes[i][0] = targets.get(i); // Setting outcome to target
-                outcomes[i][1] = targets.get(i); // Setting outcome to target
+                outcomes[i][1] = Math.abs(1-targets.get(i));// Setting outcome to target
             }
             data[i]=features; // Saving feature to data matrix
         }
@@ -79,59 +75,33 @@ public class DBNClassifier implements IERPClassifier {
         INDArray output_data = Nd4j.create(outcomes); // Create INDArray with outcomes
         INDArray input_data = Nd4j.create(data); // Create INDArray with data
         DataSet dataSet = new DataSet(input_data, output_data); // Create dataSet with input and output data
-        dataSet.shuffle(); // In place shuffle of an ndarray along a specified set of dimensions
-        dataSet.normalizeZeroMeanZeroUnitVariance(); // Subtract by the column means and divide by the standard deviation
-
-        SplitTestAndTrain testAndTrain = dataSet.splitTestAndTrain(0.90); // Spliting testing and training data
-
-        DataSet train = testAndTrain.getTrain(); // Training data
-        DataSet test = testAndTrain.getTest(); // Testing data
-        Nd4j.ENFORCE_NUMERICAL_STABILITY = true; // Setting to enforce numerical stability
 
         // Build neural net
-        build(numRows, numColumns, outputNum, seed, listenerFreq);
+        build(numRows, numColumns, seed, listenerFreq);
 
         System.out.println("Train model....");
-        model.fit(train); // Learning of neural net with training data
-
-        System.out.println("Evaluate weights....");
-        for (org.deeplearning4j.nn.api.Layer layer : model.getLayers()) {
-            INDArray w = layer.getParam(DefaultParamInitializer.WEIGHT_KEY);
-            System.out.println("Weights: " + w);
-        }
-
-        //double a = model.score(test);
-        Iterator<DataSet> iter = test.iterator(); // Initialize iterator for testing data
-        Evaluation eval =new Evaluation(outputNum); // Initialize evaluation of neural net
-        while(iter.hasNext()) {	// Iterating through testing data
-            DataSet pom = iter.next(); // One line of data
-            INDArray labels = pom.getLabels(); // Labels of each line
-            INDArray output = model.output(pom.getFeatureMatrix(), Layer.TrainingMode.TEST); // List of ground truth labels for the actual Iris species that each input sample refers to
-            //INDArray probability = model.labelProbabilities(pom.getFeatureMatrix());
-            int actual = (int)labels.getDouble(0); // Real value
-            int predict = (int)Math.round(output.getDouble(0)); // Predicted value
-            eval.eval(predict,actual); // Evaluation of prediction
-        }
-        
-        System.out.println("Evaluate model....");
-        System.out.println(eval.stats()); // Statistics of evaluation
+        model.fit(dataSet); // Learning of neural net with training data
     }
 
-    private void build(int numRows, int numColumns, int outputNum, int seed, int listenerFreq) {
+    private void build(int numRows, int outputNum, int seed, int listenerFreq) {
         System.out.print("Build model....");
+        Random rand = new Random();
+        int neuron_count = rand.nextInt(48);
+        int contrastive = rand.nextInt(15);
+        System.out.println("kontrast iter :" + contrastive + "/n" +  "pocet neuronu :" + neuron_count);
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder() // Starting builder pattern
                 .seed(seed) // Locks in weight initialization for tuning
-                .iterations(iterations) // # training iterations predict/classify & backprop
-                .learningRate(1e-6f) // Optimization step size
+                .iterations(this.iterations) // # training iterations predict/classify & backprop
+                .learningRate(0.001) // Optimization step size
                 .optimizationAlgo(OptimizationAlgorithm.CONJUGATE_GRADIENT) // Backprop to calculate gradients
-                .l1(1e-1).regularization(true).l2(2e-4) // Setting regularization, decreasing model size and speed of learning
+                .l1(0.001).regularization(true).l2(0.0001) // Setting regularization, decreasing model size and speed of learning
                 .useDropConnect(true) // Generalizing neural net, dropping part of connections
                 .list(2) // # NN layers (doesn't count input layer)
                 .layer(0, new RBM.Builder(RBM.HiddenUnit.RECTIFIED, RBM.VisibleUnit.GAUSSIAN) // Setting layer to Restricted Boltzmann machine
                         .nIn(numRows) // # input nodes
-                        .nOut(3) // # fully connected hidden layer nodes. Add list if multiple layers.
+                        .nOut(neuron_count) // # fully connected hidden layer nodes. Add list if multiple layers.
                         .weightInit(WeightInit.XAVIER) // Weight initialization
-                        .k(1) // # contrastive divergence iterations
+                        .k(3) // # contrastive divergence iterations
                         .activation("relu") // Activation function type
                         .lossFunction(LossFunction.RMSE_XENT) // Loss function type
                         .updater(Updater.ADAGRAD) // Updater type
@@ -139,7 +109,7 @@ public class DBNClassifier implements IERPClassifier {
                         .build() // Build on set configuration
                 ) // NN layer type
                 .layer(1, new OutputLayer.Builder(LossFunction.MCXENT) //Override default output layer that classifies input by Iris label using softmax
-                        .nIn(3) // # input nodes
+                        .nIn(neuron_count) // # input nodes
                         .nOut(outputNum) // # output nodes
                         .activation("softmax") // Activation function type
                         .build() // Build on set configuration
